5eb5da5680431f2d24d003e6f5a0d3c71cc3ff39
Filip_k <fkruk26@gmail.com>
2023-09-29 12:18:47 +0100

    update on main branch

--- EvenCounter.java
+++ EvenCounter.java
@@ -4,7 +4,7 @@ class EvenCounter
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello, World!");
+        System.out.println("this is another string!");
         
         for(int i = 1; i <= 20; i++) 
         {

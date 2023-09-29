b1b068ded32201afd9c90d348a8024ac8443c460
Filip_k <fkruk26@gmail.com>
2023-09-29 11:56:55 +0100

    update to simple code

---
+++ EvenCounter.java
@@ -0,0 +1,19 @@
+// Your First Program
+
+class EvenCounter
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello, World!");
+        
+        for(int i = 1; i <= 20; i++) 
+        {
+        
+            if(i % 2 == 0) 
+            {
+                System.out.println(i);  
+            }
+        }
+    }
+
+}
